417fab750b14253c7ae597e27cd700213e69067a
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-07-27 17:43:12 +0530

    Initial Commit

--- lib/splash.dart
+++ lib/splash.dart
@@ -2,39 +2,44 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Insta(),
-  ));
+  runApp(MaterialApp(home: Insta()));
 }
-
-class Insta extends StatelessWidget {
+class Insta extends StatelessWidget
+{
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context)
+  {
     return Scaffold(
-      body: Center(
+      backgroundColor: Colors.black87,
+      body:
+      Center(
         child:
         ShaderMask(
           blendMode: BlendMode.srcIn,
-          shaderCallback: (Rect bounds) => const RadialGradient(
-            center: Alignment.topCenter,
-
-
-            stops: [.5, 1,3],
+          shaderCallback: (Rect bounds) => const LinearGradient(
+            begin: Alignment.topCenter,
+            end : Alignment.bottomLeft,
             colors: [
+              Colors.purple,
               Colors.pink,
-              Colors.yellow,
-              Colors.blue,
+              Colors.redAccent,
+              Colors.orange,
+              Colors.yellowAccent,
             ],
-
-
           ).createShader(bounds),
-          child: FaIcon(FontAwesomeIcons.instagram,
-            size: 100,
-          ),
+          child:
+          const FaIcon(FontAwesomeIcons.instagram,size: 100,),
         ),
-        // FaIcon(FontAwesomeIcons.instagram),
-
       ),
     );
   }
 }
+
+
+
+// stops: [.5, 1,3],
+// colors: [
+// Colors.pink,
+// Colors.yellow,
+// Colors.blue,
+// ],
