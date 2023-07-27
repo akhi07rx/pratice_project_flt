2c797954f4f90fc768e2dcc06c3acc13115405e2
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-07-27 14:42:38 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/pratice_project_flt/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pratice_project_flt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/splash.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+void main(){
+  runApp(MaterialApp(home: Insta(),));
+
+}
+class Insta extends
+
