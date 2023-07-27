21682d00a359dfd9222040a525cfcc6683bf99ec
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-07-27 15:15:47 +0530

    Initial Commit

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,9 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-void main(){
-  runApp(MaterialApp(home: Insta(),));
-
+void main() {
+  runApp(MaterialApp(
+    home: Insta(),
+  ));
 }
-class Insta extends
 
+class Insta extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child:
+        ShaderMask(
+          blendMode: BlendMode.srcIn,
+          shaderCallback: (Rect bounds) => const RadialGradient(
+            center: Alignment.topCenter,
+
+
+            stops: [.5, 1,3],
+            colors: [
+              Colors.pink,
+              Colors.yellow,
+              Colors.blue,
+            ],
+
+
+          ).createShader(bounds),
+          child: FaIcon(FontAwesomeIcons.instagram,
+            size: 100,
+          ),
+        ),
+        // FaIcon(FontAwesomeIcons.instagram),
+
+      ),
+    );
+  }
+}
